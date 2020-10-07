c4fef89c4dc2628b7a901968ddf648c639c4f471
Trevincen <57086204+Trevincen@users.noreply.github.com>
2020-10-07 11:42:35 +0700

    Formfield di register screen

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -9,13 +9,94 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: SingleChildScrollView(
+      child: Column(
         children: [
           Container(
-            child: Text("REGISTER", style: TextStyle(fontSize: 36),),
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(top: 60, left: 24),
+              child: Text(
+                "REGISTER",
+                style: TextStyle(
+                    fontSize: 36,
+                    fontFamily: "Ubuntu",
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 29, 97, 252)),
+              )),
+          Container(
+            margin: EdgeInsets.only(top: 48, left: 24, right: 24),
+            child: TextFormField(
+              cursorColor: Theme.of(context).cursorColor,
+              decoration: InputDecoration(
+                prefixIcon:
+                    Icon(Icons.person, color: Color.fromARGB(255, 29, 97, 252)),
+                labelText: 'Full Name',
+                hintText: "Your Full Name",
+                labelStyle: TextStyle(color: Color.fromARGB(255, 29, 97, 252)),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 29, 97, 252)),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: TextFormField(
+              cursorColor: Theme.of(context).cursorColor,
+              decoration: InputDecoration(
+                prefixIcon:
+                    Icon(Icons.email, color: Color.fromARGB(255, 29, 97, 252)),
+                labelText: 'Email',
+                hintText: "Email Address",
+                labelStyle: TextStyle(color: Color.fromARGB(255, 29, 97, 252)),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 29, 97, 252)),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: TextFormField(
+              obscureText: true,
+              cursorColor: Theme.of(context).cursorColor,
+              maxLength: 20,
+              decoration: InputDecoration(
+                prefixIcon:
+                    Icon(Icons.lock, color: Color.fromARGB(255, 29, 97, 252)),
+                labelText: 'Password',
+                hintText: "Type your Password",
+                labelStyle: TextStyle(color: Color.fromARGB(255, 29, 97, 252)),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 29, 97, 252)),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: TextFormField(
+              obscureText: true,
+              cursorColor: Theme.of(context).cursorColor,
+              maxLength: 20,
+              decoration: InputDecoration(
+                prefixIcon:
+                    Icon(Icons.person, color: Color.fromARGB(255, 29, 97, 252)),
+                labelText: 'Re-Type Password',
+                hintText: "Re-Type your Password",
+                labelStyle: TextStyle(color: Color.fromARGB(255, 29, 97, 252)),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 29, 97, 252)),
+                ),
+              ),
+            ),
           )
         ],
       ),
-    );
+    ));
   }
 }
