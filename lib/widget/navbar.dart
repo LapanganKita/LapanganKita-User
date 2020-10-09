2c16f5af9743f0baff9c48b1b79b66b39de78baa
Kevin Dwi <kdwiputra@student.ciputra.ac.id>
2020-10-09 20:57:54 +0700

    Menambahkan navbar, menambahkan 4 bottom bar dan menambahkan dependency eva icons

--- lib/widget/navbar.dart
+++ lib/widget/navbar.dart
@@ -1,50 +1,113 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lapangankita_user/models/bottom_bar.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:lapangankita_user/screen/register_screen.dart';
 
-class navbar extends StatefulWidget {
+class navBar extends StatefulWidget {
   @override
-  _navbarState createState() => _navbarState();
+  _navBarState createState() => _navBarState();
 }
 
-int _currentIndex = 0;
+enum BottomIcons { Home, History, Score, Profile }
 
-final tabs = [
-  Center(child: Text('Home')),
-  Center(child: Text('History')),
-  Center(child: Text('Score')),
-  Center(child: Text('Profile')),
-];
-
-class _navbarState extends State<navbar> {
+class _navBarState extends State<navBar> {
+  BottomIcons bottomIcons = BottomIcons.Home;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Home'),
-                backgroundColor: Colors.white),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.history),
-                title: Text('History'),
-                backgroundColor: Colors.white),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.score),
-                title: Text('Score'),
-                backgroundColor: Colors.white),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                title: Text('Profile'),
-                backgroundColor: Colors.white)
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          }),
+      body: Stack(
+        children: <Widget>[
+          bottomIcons == BottomIcons.Home
+              ? Center(
+                  child: Text(
+                    "Hi, this is home page",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              : Container(),
+          bottomIcons == BottomIcons.History
+              ? Center(
+                  child: Text(
+                    "Hi, this is favorite page",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              : Container(),
+          bottomIcons == BottomIcons.Score
+              ? Center(
+                  child: Text(
+                    "Hi, this is search page",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              : Container(),
+          bottomIcons == BottomIcons.Profile
+              ? Center(
+                  child: Text(
+                    "Hi, this is account page",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              : Container(),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 24, right: 24, bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  BottomBar(
+                      onPressed: () {
+                        setState(() {
+                          bottomIcons = BottomIcons.Home;
+                        });
+                      },
+                      bottomIcons:
+                          bottomIcons == BottomIcons.Home ? true : false,
+                      icons: EvaIcons.home,
+                      text: "Home"),
+                  BottomBar(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return RegisterScreen();
+                        }));
+
+                        setState(() {
+                          bottomIcons = BottomIcons.History;
+                        });
+                      },
+                      bottomIcons:
+                          bottomIcons == BottomIcons.History ? true : false,
+                      icons: Icons.history,
+                      text: "History"),
+                  BottomBar(
+                      onPressed: () {
+                        setState(() {
+                          bottomIcons = BottomIcons.Score;
+                        });
+                      },
+                      bottomIcons:
+                          bottomIcons == BottomIcons.Score ? true : false,
+                      icons: Icons.score,
+                      text: "Score"),
+                  BottomBar(
+                      onPressed: () {
+                        setState(() {
+                          bottomIcons = BottomIcons.Profile;
+                        });
+                      },
+                      bottomIcons:
+                          bottomIcons == BottomIcons.Profile ? true : false,
+                      icons: Icons.account_circle,
+                      text: "Profile"),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
