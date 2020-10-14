e73c1ed51672173bd3251bbe6ecda0697b4a7bb9
Jeems Terri - UC <jagustinus@student.ac.id>
2020-10-14 11:12:33 +0700

    add heading text component

---
+++ lib/components/heading_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HeadingText extends StatelessWidget {
+  final _fontFamily = "Ubuntu";
+  var _fontSize = 36.0;
+  var _fontWeight = FontWeight.bold;
+  var _color;
+  final _text;
+
+  HeadingText(this._text, this._fontSize);
+  HeadingText.withColor(this._text, this._fontSize, this._color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _text,
+      style: TextStyle(
+        fontSize: _fontSize,
+        fontFamily: _fontFamily,
+        fontWeight: _fontWeight,
+        color: _color,
+      ),
+    );
+  }
+}
