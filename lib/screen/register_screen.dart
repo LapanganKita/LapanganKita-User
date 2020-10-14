1d8e30708433a198e57c1638afd92bcf1a3a6ca9
Jeems Terri - UC <jagustinus@student.ac.id>
2020-10-14 10:33:48 +0700

    fix import

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lapangankita_user/widget/navbar.dart';
+import 'package:lapangankita_user/components/navbar.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
