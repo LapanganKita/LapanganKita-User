4ba0a0868f92a4ef0f0ad30dec22e6ba002d804c
Kevin Dwi <kdwiputra@student.ciputra.ac.id>
2020-11-16 14:28:11 +0700

    updata data pada firebase.dart

--- lib/net/firebase.dart
+++ lib/net/firebase.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 Future<void> CreateUser(String name, email, password) async {
   FirebaseAuth auth = FirebaseAuth.instance;
   String uid = auth.currentUser.uid.toString();
@@ -27,5 +26,25 @@ class Joni {
     DocumentSnapshot snapshot = await users.get();
     username = snapshot.data()["name"];
     return;
+  }
+}
+
+class FetchDataLapangan {
+  final CollectionReference _lapanganList =
+      FirebaseFirestore.instance.collection("Lapangan");
+
+  Future getLapanganList() async {
+    List itemList = [];
+
+    try {
+      await _lapanganList.get().then((querysnapshot) {
+        querysnapshot.docs.forEach((element) {
+          itemList.add(element.data());
+        });
+      });
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
