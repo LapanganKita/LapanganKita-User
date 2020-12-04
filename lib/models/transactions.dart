53f6cc9b04a2315decbc3b4359130b6407ec0a24
Trevincen <57086204+Trevincen@users.noreply.github.com>
2020-12-04 19:36:29 +0700

    transaction model

---
+++ lib/models/transactions.dart
@@ -0,0 +1,19 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:equatable/equatable.dart';
+
+// class Trans extends Equatable{
+//   String partnerid;
+//   String fieldid;
+//   String time;
+//   String subtotal;
+//   String status;
+//   String couponid;
+//   String total;
+//   String ordertime;
+
+//   Trans({this.partnerid, this.fieldid, this.time, this.subtotal, this.status, this.couponid, this.total, this.ordertime});
+
+//   @override
+//   // TODO: implement props
+//   List<Object> get props => [partnerid, fieldid, time, subtotal, status, couponid, total, ordertime];
+// }
