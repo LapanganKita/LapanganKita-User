28bf9450d377b6d848abb698fac0f3c001bf6094
Kevin Dwi <kdwiputra@student.ciputra.ac.id>
2021-01-11 21:16:14 +0700

    perbaiki bagian skip button di onboarding

--- lib/screen/onboarding/onboard.dart
+++ lib/screen/onboarding/onboard.dart
@@ -6,111 +6,72 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
-  int _currentpage = 0;
-  PageController _controller = PageController();
+  final introKey = GlobalKey<IntroductionScreenState>();
 
-  List<Widget> _pages = [
-    SliderPage(
-      title: "Booking Lapangan",
-      description:
-          "ndksndksndksndksnksdsdmskdskdssklfnklsdnfklsdnflsdknfklsdfnklsdnfklsnfklsnfkls",
-      image: "assets/images/asset_login_register.png",
-    ),
-    SliderPage(
-      title: "Booking Lapangan",
-      description:
-          "ndksndksndksndksnksdsdmskdskdsfskjfksdjfksdjfklsdfklsjfklsfsdsfdsfsdfsdfsd",
-      image: "assets/images/asset_login_register.png",
-    ),
-    SliderPage(
-      title: "Booking Lapangan",
-      description:
-          "ndksndksndksndksnksdsdmskdskdfsdfsdfsfsfsfsfsdfsfsfsfsfsdfsdfsfsdfsdfsfsds",
-      image: "assets/images/asset_login_register.png",
-    )
-  ];
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => HomeScreen()),
+    );
+  }
 
-  _onChanged(int index) {
-    setState(() {
-      _currentpage = index;
-    });
+  Widget _buildImage(String assetName) {
+    return Align(
+      child:
+          Image.asset("assets/images/asset_login_register.png", width: 350.0),
+      alignment: Alignment.bottomCenter,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageView.builder(
-            scrollDirection: Axis.horizontal,
-            controller: _controller,
-            itemCount: _pages.length,
-            onPageChanged: _onChanged,
-            itemBuilder: (context, int index) {
-              return _pages[index];
-            },
-          ),
-          Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(_pages.length, (int index) {
-                    return AnimatedContainer(
-                      duration: Duration(
-                        milliseconds: 300,
-                      ),
-                      height: 10,
-                      width: (index == _currentpage) ? 30 : 10,
-                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: (index == _currentpage)
-                              ? primary_color
-                              : primary_color.withOpacity(0.5)),
-                    );
-                  }),
-                ),
-                InkWell(
-                  onTap: () {
-                    _controller.nextPage(
-                        duration: Duration(milliseconds: 800),
-                        curve: Curves.easeInOutQuint);
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) {
-                      return Wrapper();
-                    }));
-                  },
-                  child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      height: 70,
-                      alignment: Alignment.center,
-                      width: (_currentpage == (_pages.length - 1)) ? 200 : 70,
-                      decoration: BoxDecoration(
-                        color: primary_color,
-                        borderRadius: BorderRadius.circular(35),
-                      ),
-                      child: (_currentpage == (_pages.length - 1))
-                          ? Text(
-                              "Get Started",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            )
-                          : Icon(
-                              Icons.navigate_next,
-                              color: Colors.white,
-                              size: 50,
-                            )),
-                ),
-                SizedBox(
-                  height: 50,
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
     );
+
+    return IntroductionScreen(
+        key: introKey,
+        pages: [
+          PageViewModel(
+            title: "Booking Lapangan",
+            body:
+                "Instead of having to buy an entire share, invest any amount you want.",
+            image: _buildImage('img1'),
+            decoration: pageDecoration,
+          ),
+          PageViewModel(
+            title: "Booking Lapangan",
+            body:
+                "Download the Stockpile app and master the market with our mini-lesson.",
+            image: _buildImage('img2'),
+            decoration: pageDecoration,
+          ),
+          PageViewModel(
+            title: "Booking Lapangan",
+            body:
+                "Download the Stockpile app and master the market with our mini-lesson.",
+            image: _buildImage('img1'),
+            decoration: pageDecoration,
+          ),
+        ],
+        onDone: () => _onIntroEnd(context),
+        showSkipButton: true,
+        skipFlex: 0,
+        nextFlex: 0,
+        skip: const Text('Skip'),
+        next: const Icon(Icons.arrow_forward),
+        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+        dotsDecorator: const DotsDecorator(
+          size: Size(10.0, 10.0),
+          color: Color(0xFFBDBDBD),
+          activeSize: Size(22.0, 10.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          ),
+        ));
   }
 }
--- lib/screen/onboarding/onboardings.dart
+++ lib/screen/onboarding/onboardings.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:lapangankita_user/components/components.dart';
 import 'package:lapangankita_user/screen/authenticate/authenticates.dart';
+import 'package:lapangankita_user/screen/home/homes.dart';
 
 part 'onboard.dart';
 part 'login_register_onboard_screen.dart';
